fbbdc2ef89ceb9b0b74a19b2a0a0ec57ee1c4b98
aronlee5263 <aronlee5263@gmail.com>
2024-05-08 18:26:36 +0900

    6:30

--- temp/test1.jsx
+++ temp/test1.jsx
@@ -1,15 +1,34 @@
-function MyButton() {
-  return <button>I'm a button</button>;
-}
+import "./App.css";
 
-export default function MyApp() {
-  const element = <h1>JSX code</h1>;
-
+function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-      {element}
+      <header>
+        <h1>
+          <a href="/">WEB</a>
+        </h1>
+      </header>
+
+      <nav>
+        <ol>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">css</a>
+          </li>
+          <li>
+            <a href="/read/3">js</a>
+          </li>
+        </ol>
+      </nav>
+
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
+
+export default App;
